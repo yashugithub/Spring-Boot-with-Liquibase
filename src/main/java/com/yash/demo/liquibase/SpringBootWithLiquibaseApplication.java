ad1156131ddd1e86b6626b0a7a000e40ad142ab6
Yashodha Gangadharappa <yashodha.gangadharappa@microfocus.com>
2022-03-03 18:34:03 +0530

    Add initial framework

---
+++ src/main/java/com/yash/demo/liquibase/SpringBootWithLiquibaseApplication.java
@@ -0,0 +1,13 @@
+package com.yash.demo.liquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithLiquibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithLiquibaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yash/demo/liquibase/SpringBootWithLiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yash.demo.liquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithLiquibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
